16a102d6bc19f689ceb5205ae9ea84d387fdc03f
YauheniTravinau <j.travinov@gmail.com>
2023-05-21 01:40:58 +0200

    Projekt 1 React

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import '../style/Navigation.css';
 
+
+
 const Navigation = ({ isMenuOpen, handleMenuItemClick }) => {
     const [menuOpen, setMenuOpen] = useState(isMenuOpen);
 
